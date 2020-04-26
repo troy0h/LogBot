8e25010e3402dc0d8ea31a19aaf02fc860f03d10
Joshi234 <46032261+Joshi234@users.noreply.github.com>
2020-04-26 17:42:06 +0200

    Update LogBot.py

--- bot/LogBot.py
+++ bot/LogBot.py
@@ -13,7 +13,6 @@ logger.addHandler(handler)
 
 client = discord.Client()
 token = yaml.safe_load(open('token.yml'))
-imageLocation = "https://cdn.discordapp.com/attachments/703956227280994470/703974900733575278/YuzuEA.png"
 
 
 played_games=[]
@@ -28,6 +27,7 @@ async def on_message(message):
         filenametest = message.attachments[0].filename[n-3:n]
         
         if filenametest == "txt":
+            played_games.clear()
             url = message.attachments[0].url
             req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             lines = urlopen(req)
@@ -41,41 +41,40 @@ async def on_message(message):
                     cpu = output[70:]
                 elif(output[61:69]=="Host OS:"):
                     os = output[69:]
-                elif(output[94:101]=="Vulkan:"):
+                elif(output[94:101]=="Device:"):
                     renderer="Vulkan"
-                    print(renderer)
-                elif(output[106:117]=="GL_VERSION:"):
+                    gpu=output[102:]
+                  
+                elif(output[106:118]=="GL_RENDERER:"):
                     renderer="OpenGl"
-                    print(renderer)
-                elif(output[106:117]=="GL_VERSION:"):
-                    renderer="OpenGl"
-                    print(renderer)
+                    gpu=output[119:]
+                   
+
                 elif(output[59:72]=="Booting game:"):
                     played_games.append(str(output[91:]).replace("\r\n",""))
-                    
-            
-                
-        else:
-            return
-    except IndexError:
-        pass
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!EmbedTest'):
-        embed = discord.Embed(
-            title="yuzu Early Access 450",
+            isEA=False
+            embed = discord.Embed(
+            title=yuzu_version,
             description="Desc",
             color=0x00ff00)
-        embed.set_thumbnail(url=imageLocation)
-        embed.add_field(name="CPU", value="Ryzen 5 2600", inline=False)
-        embed.add_field(name="GPU", value="Radeon RX 570 Series", inline=False)
-        embed.add_field(name="OS", value="Windows 10 (10.0)", inline=False)
-        embed.add_field(name="Graphics API", value="Vulkan", inline=False)
-        await message.channel.send(embed=embed)
+            for i in str(yuzu_version).split():
+                if i=="Early":
+                    RealImageLocation="https://github.com/troy0h/LogBot/raw/master/bot/Logos/YuzuEA.png"
+                    isEA=True
+            if isEA==False:
+                RealImageLocation="https://github.com/troy0h/LogBot/raw/master/bot/Logos/Yuzu.png"
+            embed.set_thumbnail(url=RealImageLocation)
+            embed.add_field(name="CPU", value=cpu, inline=False)
+            embed.add_field(name="GPU", value=gpu, inline=False)
+            embed.add_field(name="OS", value=os, inline=False)
+            embed.add_field(name="Graphics API", value=renderer, inline=False)
+            if(len(played_games)>0):
+                embed.add_field(name="Last Played Game", value=played_games[len(played_games)-1], inline=False)
+            await message.channel.send(embed=embed)
+                    
+
+    except IndexError:
+        pass
 
 
 client.run(token["token"])
