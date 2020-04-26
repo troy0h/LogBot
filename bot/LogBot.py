df41c92fbb2c263da69b465907d0cf0454f57cd2
Troy Hull <troy0h@hotmail.co.uk>
2020-04-26 15:36:04 +0100

    Added a bot embed test

--- bot/LogBot.py
+++ bot/LogBot.py
@@ -13,6 +13,7 @@ logger.addHandler(handler)
 
 client = discord.Client()
 token = yaml.safe_load(open('token.yml'))
+imageLocation = "https://cdn.discordapp.com/attachments/703956227280994470/703974900733575278/YuzuEA.png"
 
 @client.event
 async def on_ready():
@@ -32,5 +33,23 @@ async def on_message(message):
             return
     except IndexError:
         pass
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!EmbedTest'):
+        embed = discord.Embed(
+            title="yuzu Early Access 450",
+            description="Desc",
+            color=0x00ff00)
+        embed.set_thumbnail(url=imageLocation)
+        embed.add_field(name="CPU", value="Ryzen 5 2600", inline=False)
+        embed.add_field(name="GPU", value="Radeon RX 570 Series", inline=False)
+        embed.add_field(name="OS", value="Windows 10 (10.0)", inline=False)
+        embed.add_field(name="Graphics API", value="Vulkan", inline=False)
+        await message.channel.send(embed=embed)
+
 
 client.run(token["token"])
