2a9699f9fe49889693da0045ad7153d356ccd7e3
Troy Hull <troy0h@hotmail.co.uk>
2020-04-26 17:36:40 +0100

    General Cleanup

--- bot/LogBot.py
+++ bot/LogBot.py
@@ -15,8 +15,6 @@ logger.addHandler(handler)
 client = discord.Client()
 token = yaml.safe_load(open('token.yml'))
 
-
-
 played_games=[]
 
 @client.event
@@ -31,7 +29,6 @@ async def on_message(message):
     try:
         n = len(message.attachments[0].filename)
         filenametest = message.attachments[0].filename[n-3:n]
-        print(filenametest)
         if filenametest == "txt":
             played_games.clear()
             url = message.attachments[0].url
@@ -43,43 +40,39 @@ async def on_message(message):
                 if(output[61:74]=="yuzu Version:"):
                     end_index=str(output).find("| HEAD")
                     yuzu_version = output[74:end_index]
-                    print(yuzu_version)
                 elif(output[61:70]=="Host CPU:"):
                     cpu = output[70:]
-                    print(cpu)
                 elif(output[61:69]=="Host OS:"):
                     os = output[69:]
                 elif(output[94:101]=="Device:"):
                     renderer="Vulkan"
-                    gpu=output[102:]
-                  
+                    gpu = output[102:]
                 elif(output[106:118]=="GL_RENDERER:"):
                     renderer="OpenGl"
-                    gpu=output[119:]
-                   
-
+                    gpu = output[119:]
+                elif(output[61:83]=="System_UseDockedMode:"):
+                    docked = output[84:]
                 elif(output[59:72]=="Booting game:"):
                     played_games.append(str(output[91:]).replace("\r\n",""))
+
             isEA=False
-            embed = discord.Embed(
-            title=yuzu_version,
-            description="Desc",
-            color=0x00ff00)
+
             for i in str(yuzu_version).split():
                 if i=="Early":
                     RealImageLocation="https://github.com/troy0h/LogBot/raw/master/bot/Logos/YuzuEA.png"
                     isEA=True
             if isEA==False:
                 RealImageLocation="https://github.com/troy0h/LogBot/raw/master/bot/Logos/Yuzu.png"
+            embed = discord.Embed(title=yuzu_version,color=0x00ff00)
             embed.set_thumbnail(url=RealImageLocation)
             embed.add_field(name="CPU", value=cpu, inline=False)
             embed.add_field(name="GPU", value=gpu, inline=False)
-            embed.add_field(name="OS", value=os, inline=False)
-            embed.add_field(name="Graphics API", value=renderer, inline=False)
+            embed.add_field(name="OS", value=os, inline=True)
+            embed.add_field(name="Graphics API", value=renderer, inline=True)
+            embed.add_field(name="Docked", value=docked, inline=True)
             if(len(played_games)>0):
-                embed.add_field(name="Last Played Game", value=played_games[len(played_games)-1], inline=False)
-            await message.channel.send(embed=embed)
-                    
+                embed.add_field(name="Last Played Game", value=played_games[len(played_games)-1], inline=True)
+            await message.channel.send(embed=embed)                    
 
     except IndexError:
         pass
